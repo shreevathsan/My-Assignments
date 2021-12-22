bed2faf4c07888ec07cb6245421cd9cdf721c8a4
Shreevathsan <shreevathsan7@gmail.com>
2021-12-22 21:59:17 +0530

    First Assignement

---
+++ Assignment.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Assignment
+{
+	public static void main(String args[])
+	
+	{
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		c=a+b;
+		System.out.println(c);
+	}
+}
